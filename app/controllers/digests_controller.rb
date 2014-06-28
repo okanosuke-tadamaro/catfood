06069db68f2913b8d5590e507267d85abc29c207
Justin Tanacredi <Jtanacredi@gmail.com>
2014-06-28 16:41:15 -0400

    add twilio message in digests controller

--- app/controllers/digests_controller.rb
+++ app/controllers/digests_controller.rb
@@ -3,5 +3,19 @@ class DigestsController < ApplicationController
 
 	def create
     render nothing: true, status: 200
-	end
+
+    account_sid = ENV[AccSID],
+    auth_token = ENV[AuthToken]
+
+
+
+    @client = Twilio::REST::Client.new account_sid, auth_token
+
+    @client.account.messages.create({
+                                        :from => '+15617392747',
+                                        :to => phone_num,
+                                        :body => "Hi #{email}, You should eat your next meal at #{time}. Try eating some #{rec}"
+                                    })
+  end
+
 end
