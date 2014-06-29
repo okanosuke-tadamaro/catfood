dee99f914691950b5752027ea612fe230d1071d0
Brooks Swinnerton <bswinnerton@gmail.com>
2014-06-28 21:52:22 -0400

    Adding suggestion scaffolding

---
+++ app/controllers/suggestions_controller.rb
@@ -0,0 +1,4 @@
+class SuggestionsController < ApplicationController
+  def show
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { :omniauth_callbacks => 'users/omniauth_callbacks', :sessions => 'sessions' }
+
   post 'digest' => 'digests#create'
+  get 'suggestions/:id' => 'suggestions#show'
   get '/about' => 'splash#about'
+
   root 'splash#index'
 end
