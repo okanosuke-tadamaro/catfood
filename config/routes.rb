f301455303d5b1a9d289ef79dddc23c97073c32b
originalblend337 <originalblend337@gmail.com>
2014-06-28 21:46:23 -0400

    added about page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { :omniauth_callbacks => 'users/omniauth_callbacks', :sessions => 'sessions' }
   post 'digest' => 'digests#create'
+  get '/about' => 'splash#about'
   root 'splash#index'
 end
